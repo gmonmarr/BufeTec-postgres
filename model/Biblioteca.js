b2a4418711fc7f9eecf629fd690a37be974f5ea0
Guillermo Montemayor <A01722402@tec.mx>
2024-10-12 17:25:20 -0600

    Updated biblioteca to correctly export itself.

--- model/Biblioteca.js
+++ model/Biblioteca.js
@@ -1,27 +1,33 @@
+// ./model/Biblioteca.js
+
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const File = sequelize.define('File', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Biblioteca = sequelize.define(
+  "Biblioteca",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    descripcion: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    url_del_pdf: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
   },
-  descripcion:{
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  url_del_pdf: {
-    type: DataTypes.TEXT,
-    allowNull: false
+  {
+    tableName: "Biblioteca",
+    timestamps: true,
   }
-},{
-    tableName: 'Files',
-    timestamps: true
-});
+);
 
-module.exports = File;
+module.exports = Biblioteca;
